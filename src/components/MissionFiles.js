ca9a686f351dab692ba46e4057948ec6d7317b80
Ludivine <ludivine.pellissier@gmail.com>
2018-11-25 20:05:12 +0100

    add upload on student side + add display studentfiles on lawyer and admin sides

--- src/components/MissionFiles.js
+++ src/components/MissionFiles.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 const MissionFiles = ({files, download}) => {
-return files.map(file => 
-<div>
-  <span>{file.name}</span>
-  <span style={{cursor: 'pointer'}} onClick={() => download(file.id)}>Télécharger</span>
-</div>)
+  const showFiles = files => {
+    if (files.length === 0) {
+      return <div><span>Aucun fichier n'a encore été partagé.</span></div>
+    } else {
+      return files.map(file => 
+        <div>
+          <span>{file.name}</span>
+          <span style={{cursor: 'pointer'}} onClick={() => download(file.id)}>Télécharger</span>
+        </div>
+      )
+    } 
+  }
+
+  return (
+    <div>
+      <div>{showFiles(files)}</div>
+    </div>
+  )
 }
 
 export default MissionFiles
